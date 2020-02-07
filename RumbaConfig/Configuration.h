84434745738758f4ed5aa1920b54247b55bbbf7d
Andrey Yaromenok/gangutlx <ayaromenok@gmail.com>
2020-02-07 11:24:03 +0200

    #2 update bed size to 160x160

--- RumbaConfig/Configuration.h
+++ RumbaConfig/Configuration.h
@@ -1059,8 +1059,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 160
+#define Y_BED_SIZE 160
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
